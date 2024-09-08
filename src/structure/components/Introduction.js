79036541dca1ee6dd304925f1c2e583e5521a8c0
Miguel <miguel87666@gmail.com>
2024-09-08 17:19:58 +0100

    fix: fixed informations

--- src/structure/components/Introduction.js
+++ src/structure/components/Introduction.js
@@ -4,6 +4,17 @@ import LinkedInLogo from '../../resources/images/linkedin-brands.svg'
 
 export default class Introduction extends Component {
   render() {
+    function getAge(dateString) {
+      var today = new Date();
+      var birthDate = new Date(dateString);
+      var age = today.getFullYear() - birthDate.getFullYear();
+      var m = today.getMonth() - birthDate.getMonth();
+      if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
+        age--;
+      }
+      return age;
+    }
+
     return (
       <div id="introduction" className="introduction pb-5 pb-sm-0 bg-white">
         <div className="container">
@@ -11,15 +22,15 @@ export default class Introduction extends Component {
             <div className="col-12 col-sm-8 pr-sm-5">
               <div className="intro-title pb-3 pt-5 pt-sm-0">About myself</div>
               <p className="text-justify">
-                My name is Miguel Santos and I'm a Web developer. I started developing around 2017 in my programming course. 
-                I like to develop with React but I had the chance to work with other tecnologies throughout my career. 
+                My name is Miguel Santos and I'm a Web developer. I started developing around 2017 in my programming course.
+                I like to develop with React but I had the chance to work with other tecnologies throughout my career.
                 You can see more about my projects swipping down.
               </p>
             </div>
             <div className="col-12 col-sm-4 pt-5 pt-sm-0 pl-sm-5">
               <div className="intro-title pb-3">Details</div>
               <div><b>Age</b></div>
-              <div className="gray pb-2">24 years old</div>
+              <div className="gray pb-2">{getAge("1996-11-15")}</div>
               <div className="pt-2"><b>Location</b></div>
               <div className="gray">Portugal, Setúbal, Moita</div>
 
@@ -33,7 +44,7 @@ export default class Introduction extends Component {
           </div>
 
         </div>
-        
+
       </div>
     );
   }
--- src/structure/components/Projects.js
+++ src/structure/components/Projects.js
@@ -19,48 +19,48 @@ export default class Projects extends Component {
                   <div className="company-start-date">07/2023</div>
                   <span> - </span>
                   <div className="company-end-date">01/2024</div>
-                 </div>
+                </div>
                 <div className="company-name">Bliss Applications</div>
               </div>
             </div>
             <div className="col-sm-9 col-12 text-justify text-sm-left">
-            <div className="project-container">
-            <div className="project">
-              <div className="project-description pt-5">
-                <p>Development of <mark>Cartier</mark> website:
-                Development of new features and bug fixing. The development was made with React, Typescript and Styled Components by following a Zeplin design with pixel perfect. 
-                The methodology used was SCRUM with daily meetings and 2 week Sprints. For unit testing Jest was used with React testing library.
-                </p>
-                <p>Tecnologies used:</p>
-                <ul>
-                  <li>React</li>
-                  <li>Typescript</li>
-                  <li>Styled Components</li>
-                  <li>Storybook</li>
-                  <li>Jest and RTL</li>
-                  <li>JIRA and Confluence</li>
-                  <li>GIT</li>
-                  <li>CI/CD</li>
-                  <li>REST API</li>
-                </ul>
-                </div>
-              </div>
-
-              <div className="project pt-5">
-                <div className="project-img"></div>
-                <div className="project-description pt-5">
-                <p>Development of <mark>Banks Institutional Websites</mark> using a CMS. The project consists in creating the website according to the design team drawing. Development of an exchange widget component hosted by an HTTP Application Server.</p>
-                <p>Tecnologies used:</p>
-                <ul>
-                  <li>dotCMS</li>
-                  <li>JQuery</li>
-                  <li>HTML, CSS and Bootstrap</li>
-                </ul>
+              <div className="project-container">
+                <div className="project">
+                  <div className="project-description pt-5">
+                    <p>Development of an <mark>E-Commerce</mark> website:
+                      Development of new features and bug fixing. The development was made with React, Typescript and Styled Components by following a Zeplin design with pixel perfect.
+                      The methodology used was SCRUM with daily meetings and 2 week Sprints. For unit testing Jest was used with React testing library.
+                    </p>
+                    <p>Tecnologies used:</p>
+                    <ul>
+                      <li>React</li>
+                      <li>Typescript</li>
+                      <li>Styled Components</li>
+                      <li>Storybook</li>
+                      <li>Jest and RTL</li>
+                      <li>JIRA and Confluence</li>
+                      <li>GIT</li>
+                      <li>CI/CD</li>
+                      <li>REST API</li>
+                    </ul>
+                  </div>
                 </div>
 
-              </div>
+                <div className="project pt-5">
+                  <div className="project-img"></div>
+                  <div className="project-description pt-5">
+                    <p>Development of <mark>Banks Institutional Websites</mark> using a CMS. The project consists in creating the website according to the design team drawing. Development of an exchange widget component hosted by an HTTP Application Server.</p>
+                    <p>Tecnologies used:</p>
+                    <ul>
+                      <li>dotCMS</li>
+                      <li>JQuery</li>
+                      <li>HTML, CSS and Bootstrap</li>
+                    </ul>
+                  </div>
 
-            </div>
+                </div>
+
+              </div>
 
             </div>
           </div>
@@ -71,47 +71,47 @@ export default class Projects extends Component {
                   <div className="company-start-date">09/2018</div>
                   <span> - </span>
                   <div className="company-end-date">07/2023</div>
-                 </div>
+                </div>
                 <div className="company-name">Asseco PST</div>
               </div>
             </div>
             <div className="col-sm-9 col-12 text-justify text-sm-left">
-            <div className="project-container">
-            <div className="project">
-              <div className="project-description pt-5">
-                <p>Development of a <mark>Banking</mark> platform (React):
-                Development of the front-end using React. The project consists in creating a banking platform solution using React (Functional) with the help of Material UI. The tasks are defined in JIRA and the documentation in Confluence. The project uses GIT for version controlling and collaboration, using the Gitflow methodology. 
-                The front-end and back-end communications are made with RESTfull webservices. In the front-end the data is retrieved accessing the API endpoints in JSON. 
-                The APIs are tested using Swagger. The application server is deployed in NGINX.
-                </p>
-                <p>Tecnologies used:</p>
-                <ul>
-                  <li>React</li>
-                  <li>Material UI</li>
-                  <li>JIRA and Confluence</li>
-                  <li>GIT</li>
-                  <li>REST</li>
-                  <li>Swagger</li>
-                  <li>API Endpoints</li>
-                </ul>
-                </div>
-              </div>
-
-              <div className="project pt-5">
-                <div className="project-img"></div>
-                <div className="project-description pt-5">
-                <p>Development of <mark>Banks Institutional Websites</mark> using a CMS. The project consists in creating the website according to the design team drawing. Development of an exchange widget component hosted by an HTTP Application Server.</p>
-                <p>Tecnologies used:</p>
-                <ul>
-                  <li>dotCMS</li>
-                  <li>JQuery</li>
-                  <li>HTML, CSS and Bootstrap</li>
-                </ul>
+              <div className="project-container">
+                <div className="project">
+                  <div className="project-description pt-5">
+                    <p>Development of a <mark>Banking</mark> platform (React):
+                      Development of the front-end using React. The project consists in creating a banking platform solution using React (Functional) with the help of Material UI. The tasks are defined in JIRA and the documentation in Confluence. The project uses GIT for version controlling and collaboration, using the Gitflow methodology.
+                      The front-end and back-end communications are made with RESTfull webservices. In the front-end the data is retrieved accessing the API endpoints in JSON.
+                      The APIs are tested using Swagger. The application server is deployed in NGINX.
+                    </p>
+                    <p>Tecnologies used:</p>
+                    <ul>
+                      <li>React</li>
+                      <li>Material UI</li>
+                      <li>JIRA and Confluence</li>
+                      <li>GIT</li>
+                      <li>REST</li>
+                      <li>Swagger</li>
+                      <li>API Endpoints</li>
+                    </ul>
+                  </div>
                 </div>
 
-              </div>
+                <div className="project pt-5">
+                  <div className="project-img"></div>
+                  <div className="project-description pt-5">
+                    <p>Development of <mark>Banks Institutional Websites</mark> using a CMS. The project consists in creating the website according to the design team drawing. Development of an exchange widget component hosted by an HTTP Application Server.</p>
+                    <p>Tecnologies used:</p>
+                    <ul>
+                      <li>dotCMS</li>
+                      <li>JQuery</li>
+                      <li>HTML, CSS and Bootstrap</li>
+                    </ul>
+                  </div>
 
-            </div>
+                </div>
+
+              </div>
 
             </div>
           </div>
@@ -126,67 +126,52 @@ export default class Projects extends Component {
                 </div>
                 <div className="company-name">NO SHAPE</div>
               </div>
-              
+
             </div>
             <div className="col-sm-9 col-12 text-justify text-sm-left">
-            <div className="project-container">
-              <div className="project">
-              <div className="project-description">
-                <p>Development of an <mark>internal prize rewards</mark> web application. The development was made using .NET, Javascript, HTML, Bootstrap and CSS. The database was SQL Server.</p>
-                <p>Tecnologies used:</p>
-                <ul>
-                  <li>.NET</li>
-                  <li>Javascript</li>
-                  <li>JQuery</li>
-                  <li>HTML, CSS and Bootstrap</li>
-                  <li>SQL Server</li>
-                </ul>
+              <div className="project-container">
+                <div className="project">
+                  <div className="project-description">
+                    <p>Development of an <mark>internal prize rewards</mark> web application. The development was made using .NET, Javascript, HTML, Bootstrap and CSS. The database was SQL Server.</p>
+                    <p>Tecnologies used:</p>
+                    <ul>
+                      <li>.NET</li>
+                      <li>Javascript</li>
+                      <li>JQuery</li>
+                      <li>HTML, CSS and Bootstrap</li>
+                      <li>SQL Server</li>
+                    </ul>
+                  </div>
+
+                </div>
+                <div className="project pt-5">
+                  <div className="project-description pt-5">
+                    <p>Development of a <mark>Image and Video gallery Website</mark> front-end. The development was made using .NET, Javascript, JQuery, HTML, Bootstrap and CSS.</p>
+                    <p>Tecnologies used:</p>
+                    <ul>
+                      <li>.NET</li>
+                      <li>Javascript</li>
+                      <li>JQuery</li>
+                      <li>HTML, CSS and Bootstrap</li>
+                    </ul>
+                  </div>
                 </div>
 
-              </div>
-
-              <div className="project pt-5">
-              <div className="project-description pt-5">
-                <p>Development of the <mark>Portuguese Football Federation</mark> fans and associates Website (Portugal +) front-end (design). The development was made using Javascript, JQuery, HTML, Bootstrap and CSS.
-  
-                  </p>
-                <p>Tecnologies used:</p>
-                <ul>
-                  <li>Javascript</li>
-                  <li>JQuery</li>
-                  <li>HTML, CSS and Bootstrap</li>
-                </ul>
+                <div className="project pt-5">
+                  <div className="project-description pt-5">
+                    <p>Maintenance of client <mark>web application</mark>. The software was developed in PHP (Laravel), Javascript, HTML and CSS. The project consisted in bug fixing mainly in the front-end.</p>
+                    <p>Tecnologies used:</p>
+                    <ul>
+                      <li>PHP (Laravel)</li>
+                      <li>Javascript</li>
+                      <li>JQuery</li>
+                      <li>HTML, CSS and Bootstrap</li>
+                    </ul>
+                  </div>
                 </div>
+
+
               </div>
-
-              <div className="project pt-5">
-              <div className="project-description pt-5">
-                <p>Development of a <mark>Image and Video gallery Website</mark> front-end. The development was made using .NET, Javascript, JQuery, HTML, Bootstrap and CSS.</p>
-                <p>Tecnologies used:</p>
-                <ul>
-                  <li>.NET</li>
-                  <li>Javascript</li>
-                  <li>JQuery</li>
-                  <li>HTML, CSS and Bootstrap</li>
-                </ul>
-                </div>
-              </div>
-
-              <div className="project pt-5">
-              <div className="project-description pt-5">
-                <p>Maintenance of client <mark>web application</mark>. The software was developed in PHP (Laravel), Javascript, HTML and CSS. The project consisted in bug fixing mainly in the front-end.</p>
-                <p>Tecnologies used:</p>
-                <ul>
-                  <li>PHP (Laravel)</li>
-                  <li>Javascript</li>
-                  <li>JQuery</li>
-                  <li>HTML, CSS and Bootstrap</li>
-                </ul>
-                </div>
-              </div>
-
-
-            </div>
 
             </div>
           </div>
@@ -196,45 +181,18 @@ export default class Projects extends Component {
               <div className="company-data">
                 <div className="company-name">Education and Courses</div>
               </div>
-              
+
             </div>
             <div className="col-sm-9 col-12 text-justify text-sm-left">
-            <div className="project-container">
-              <div className="project">
-              <div className="project-description">
-                <p className="company-date">2016 - 2017</p>
-                <p><b>ATEC</b></p>
-                <p>Technical Specialist In Technologies And Systems Programming</p>
+              <div className="project-container">
+                <div className="project">
+                  <div className="project-description">
+                    <p className="company-date">2016 - 2017</p>
+                    <p><b>ATEC</b></p>
+                    <p>Technical Specialist In Technologies And Systems Programming</p>
+                  </div>
                 </div>
-
               </div>
-              <div className="project">
-              <div className="project-description pt-5">
-                <p className="company-date">2019</p>
-                <p><b>Affinity</b></p>
-                <p>Affinity React Intermediate Course</p>
-                </div>
-
-              </div>
-              <div className="project">
-              <div className="project-description pt-5">
-                <p className="company-date">2019</p>
-                <p><b>Affinity</b></p>
-                <p>Affinity React Begginer Course</p>
-                </div>
-
-              </div>
-              <div className="project">
-              <div className="project-description pt-5">
-                <p className="company-date">2019</p>
-                <p><b>Affinity</b></p>
-                <p>Affinity Javascript ES6 Course</p>
-                </div>
-
-              </div>
-
-
-            </div>
 
             </div>
           </div>
